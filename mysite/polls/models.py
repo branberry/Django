aebe0ef04529360f7cf390f150f04aefcb335b26
wowbob396 <wowbob396@gmail.com>
2017-07-01 21:42:06 -0500

    first updates

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    questionText = models.CharField(max_length=200)
+    pubDate = models.DateTimeField('Date Published')
+
+class Choice(models.Model):
+    # ForeignKey represents the connection between the two classes
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choiceText = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
