6b166f7c9475f86a07ab82599c588a25bb9aee16
wowbob396 <wowbob396@gmail.com>
2017-07-02 22:24:18 -0500

    worked on database

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,13 +1,24 @@
 from django.db import models
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
 class Question(models.Model):
     questionText = models.CharField(max_length=200)
     pubDate = models.DateTimeField('Date Published')
+    def __str__(self):
+        return self.questionText
+    def was_published_recently(self):
+        return self.pubDate >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     # ForeignKey represents the connection between the two classes
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choiceText = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choiceText
 
